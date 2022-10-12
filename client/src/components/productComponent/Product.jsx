889de27f916c9e8f4a56bc186b4b5454d58c9f54
Nicholas Amenda <Nickamenda@gmail.com>
2022-10-12 15:37:38 -0500

    create starReview comp

--- client/src/components/productComponent/Product.jsx
+++ client/src/components/productComponent/Product.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import StarReview from './StarReview.jsx'
 
 const Product = () => {
   return (
-    <div>test</div>
+    <>
+      <div className="product-container"></div>
+      <StarReview />
+    </>
+
   )
 }
-
 export default Product;
