4cb752b442ec7a50bdd0411be5a66eaa1dfe0f9a
Nicholas Amenda <Nickamenda@gmail.com>
2022-10-14 17:29:42 -0500

    add more info and style more

--- client/src/components/productComponent/Product.jsx
+++ client/src/components/productComponent/Product.jsx
@@ -9,34 +9,35 @@ import stylesExample from './exampleData/styles.js'
 const Product = ({ product }) => {
 
 
-    const [currentProduct, setCurrentProduct] = useState(product);
-    const [styles, setStyles] = useState([]);
-    const [currentStyle, setCurrentStyle] = useState({})
-    const [currentPhoto, setCurrentPhoto] = useState('')
-    useEffect(() => {
-      axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfc/products/${product.id}/styles`, {
-        headers: {
-          'Authorization': process.env.AUTH_KEY
-        }
-      })
+  const [currentProduct, setCurrentProduct] = useState(product);
+  const [styles, setStyles] = useState([]);
+  const [currentStyle, setCurrentStyle] = useState({})
+  const [currentPhoto, setCurrentPhoto] = useState('')
+  const [zoom, setZoom] = useState(false);
+  useEffect(() => {
+    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfc/products/${product.id}/styles`, {
+      headers: {
+        'Authorization': process.env.AUTH_KEY
+      }
+    })
       .then((res) => {
         setStyles(res.data.results)
         setCurrentStyle(res.data.results[0])
         handleCurrentPhoto(res.data.results[0].photos[0])
       })
 
-    }, [])
-    function handleStyles(style) {
-      setCurrentStyle(style)
-    }
+  }, [])
+  function handleStyles(style) {
+    setCurrentStyle(style)
+  }
 
-    function handleCurrentPhoto(item) {
-      setCurrentPhoto(item.url)
-    }
-    function handleSales(item) {
-      if (item.sale_price) {
-        return (
-          <>
+  function handleCurrentPhoto(item) {
+    setCurrentPhoto(item.url)
+  }
+  function handleSales(item) {
+    if (item.sale_price) {
+      return (
+        <>
           <div className="product old-price" style={{ color: item.sale_price ? 'red' : 'black', textDecoration: item.sale_price ? 'line-through' : "none" }}>${item.original_price}</div>
           <div className="product current-price">${item.sale_price}</div>
         </>
@@ -44,10 +45,10 @@ const Product = ({ product }) => {
     }
     return (
       <div className="product old-price">${item.original_price}</div>
-      )
-    }
-    return Object.keys(currentStyle).length !== 0 ? (
-      <>
+    )
+  }
+  return Object.keys(currentStyle).length !== 0 ? (
+    <>
       <div className="product container">
         <div className="product current-photos">
           <div className="product thumbnails">
@@ -57,10 +58,13 @@ const Product = ({ product }) => {
                   e.preventDefault();
                   handleCurrentPhoto(item)
                 }}></img>
-                )
-              })}
+              )
+            })}
           </div>
-          <img className="product mainPic" src={currentPhoto} alt={currentStyle.name}></img>
+          <img className="product mainPic" style={{width: zoom ? '750px' : null, height: zoom ? '750px' : null}} src={currentPhoto} alt={currentStyle.name} onClick={(e) => {
+            e.preventDefault();
+            setZoom(!zoom)
+          }}></img>
         </div>
         <div className="product current-info">
           <div className="product reviews">
@@ -74,10 +78,21 @@ const Product = ({ product }) => {
           <CartInfo currentStyle={currentStyle} />
         </div>
       </div>
-        <div className="product bottom-info">
-          <div className="product current-slogan">{currentProduct.slogan}</div>
-          <div className="product current-description">{currentProduct.description}</div>
-          </div>
+      <div className="product bottom-info">
+        <div className="product bottom-left">
+        <div className="product current-slogan">{currentProduct.slogan}</div>
+        <div className="product current-description">{currentProduct.description}</div>
+        </div>
+      <div className="product features">{
+        currentProduct.features.map((item) => {
+          return (
+            <>
+            <div className="product featureItem">{item.feature}: {item.value}</div>
+            </>
+          )
+        })
+      }</div>
+      </div>
     </>
   ) : null
 }
