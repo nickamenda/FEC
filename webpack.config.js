b60271c6b4addad4c7228fecfa1e4295826fba23
Cameron Montgomery <jc.montgomery0102@gmail.com>
2022-10-14 09:56:38 -0600

    fixed plugin for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -30,8 +30,6 @@ module.exports = {
                 {
                   "regenerator": true
                 }
-              ],[
-              new Dotenv()
               ]
             ]
           }
