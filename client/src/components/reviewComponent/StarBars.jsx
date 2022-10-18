f3a70c138d29e9937140f830ee0bef41efe945de
Jonathan Liang <Jonathanliang15@gmail.com>
2022-10-18 14:02:14 -0500

    Creating the bars

--- client/src/components/reviewComponent/StarBars.jsx
+++ client/src/components/reviewComponent/StarBars.jsx
@@ -2,36 +2,10 @@ import React, {useState, useEffect} from 'react';
 
 const StarBars = (props) => {
 
-  let maxItem = 0;
 
-
-
-  if (props.ratings) {
-    for (let i = 1; i < 6; i++) {
-      if (!props.ratings[i]) {
-        props.ratings[i] = '0'
-      }
-    }
-    console.log('props.ratings:', props.ratings)
-    let maxNum = Number(props.ratings[1]);
-    for (let key in props.ratings) {
-      if (maxNum < Number(props.ratings[key])) {
-        maxNum = Number(props.ratings[key])
-      }
-    }
-    maxItem = maxNum;
-  }
-
-  // setTimeout(() => {
-  //   console.log(props.ratings)
-  // }, 2000)
   return (
     <>
-    <div>
-      <div>
-        <span></span>
-      </div>
-    </div>
+      <p>hello</p>
       {/* <span>{props.ratings[1]}</span>
       <span>{props.ratings[2]}</span>
       <span>{props.ratings[3]}</span>
--- client/src/components/reviewComponent/StarsRating.jsx
+++ client/src/components/reviewComponent/StarsRating.jsx
@@ -8,6 +8,8 @@ const StarsRating = (props) => {
   const [ratings, setRatings] = useState({})
   const [recommend, setRecommend] = useState({})
 
+  const arrOfRatings = [];
+
   useEffect(() => {
     axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfc/reviews/meta?product_id=${props.productId}`, {
       headers: {
@@ -25,9 +27,6 @@ const StarsRating = (props) => {
   // ratings.characteristics
   // ratings.ratings
   // ratings.recommended
-  useEffect(() => {
-    console.log(recommend)
-  }, [recommend])
 
   // Creating the Average Rating points
   var totalNumReviews = 0; // total number of reviews
@@ -40,6 +39,34 @@ const StarsRating = (props) => {
     }
   }
 
+  let maxItem = 0;
+
+  if (ratings) {
+    // checking if there are any missing numbers and if make it 0
+    for (let i = 1; i < 6; i++) {
+      if (!ratings[i]) {
+        ratings[i] = '0'
+      }
+    }
+    console.log('props.ratings:', ratings)
+    // getting the largest value
+    let maxNum = Number(ratings[1]);
+    for (let key in ratings) {
+      if (maxNum < Number(ratings[key])) {
+        maxNum = Number(ratings[key])
+      }
+    }
+    maxItem = maxNum;
+    // creating an array of objs for StarBars
+    for (let j = 1; j < 6; j++) {
+      let obj = {}
+      obj['number'] = j;
+      obj['value'] = ratings[j];
+      arrOfRatings.push(obj)
+    }
+    console.log(arrOfRatings)
+  }
+
 
 
   return (
@@ -47,7 +74,9 @@ const StarsRating = (props) => {
       <p>Star Average</p>
       <p>{(totalRatingPoints / totalNumReviews).toFixed(1)}</p>
       <p>{Math.ceil(((recommend.false / recommend.true).toFixed(2) * 100))}% of reviews recommend this product</p>
-      <StarBars ratings={ratings}/>
+      {arrOfRatings.length > 0 ? arrOfRatings.map((rating, id) => (
+        <StarBars key={id} rating={rating}/>
+      )) : null}
     </>
   )
 }
