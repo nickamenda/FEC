8e61c79c8765db543715ae7016e65248fb3b8ef0
Cameron Montgomery <jc.montgomery0102@gmail.com>
2022-10-19 14:02:53 -0600

    Added layout styling; finished AnswerFooter sub component

--- client/src/components/questionsComponent/QAEntryComponents/AnswerFooter.jsx
+++ client/src/components/questionsComponent/QAEntryComponents/AnswerFooter.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import Helpful from './Helpful.jsx';
 import Axios from 'axios';
 import { format, parseISO } from 'date-fns';
+import Helpful from './Helpful.jsx';
+import ReportBtn from './ReportBtn.jsx';
 
 const AnswerFooter = ({ answer }) => {
 
   return (
     <div className="QA-answer-footer">
-      by {answer.answerer_name}, {format(parseISO(answer.date), 'MMM d\, YYY')} |
-      <Helpful helpfulCount={answer.helpfulness} options={{type: 'answers', id: answer.id}} />
+      <span className="QA-answer-by-date">by {answer.answerer_name}, {format(parseISO(answer.date), 'MMM d\, YYY')}</span>
+      |<Helpful className="QA-answer-helpful" helpfulCount={answer.helpfulness} options={{type: 'answers', id: answer.id}} />
+      |<ReportBtn options={{type: 'answers', id: answer.id}} />
     </div>
   )
 }
--- client/src/components/questionsComponent/QAList.jsx
+++ client/src/components/questionsComponent/QAList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import QAEntry from './QAEntry.jsx';
 
 const QAList = ({ questions }) => {
