9f9802daa8b7340c5c2b32cfd80b3aba4a927ca6
Cameron Montgomery <jc.montgomery0102@gmail.com>
2022-10-19 17:42:21 -0600

    Added support to show images if provided in answer;

--- client/src/components/questionsComponent/QAEntryComponents/AnswerEntry.jsx
+++ client/src/components/questionsComponent/QAEntryComponents/AnswerEntry.jsx
@@ -8,6 +8,11 @@ const AnswerEntry = ({ answer }) => {
     <div className="QA-answer-body">
       {answer.body}
     </div>
+    <>
+    {answer.photos.length !== 0 ? answer.photos.map((photo, i) => {
+        return <img src={photo} key={i} className="QA-answer-img" alt="Pictures provided by poster" />
+      }) : null}
+    </>
     <AnswerFooter answer={answer}/>
   </div>
   )
