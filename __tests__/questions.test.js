476d4520eac120a7e8ccfbfe76b68cadcc62271d
Cameron Montgomery <jc.montgomery0102@gmail.com>
2022-10-18 21:24:48 -0600

    Installed dependeces and set up config file. Running into the JSDom issue again...

---
+++ __tests__/questions.test.js
@@ -0,0 +1,8 @@
+import {render, screen} from '@testing-library/react';
+import React from 'react';
+import Question from '../client/src/components/questionsComponent/Question.jsx';
+
+it("Should show question component heading", () => {
+  render(<Question />);
+  const heading = screen.getByTestId('question-header');
+});
