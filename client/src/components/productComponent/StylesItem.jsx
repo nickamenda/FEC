48cb465980987101fa0a2d416bad458c32b166a9
Nicholas Amenda <Nickamenda@gmail.com>
2022-10-25 11:39:58 -0500

    clean up stylesItem

--- client/src/components/productComponent/StylesItem.jsx
+++ client/src/components/productComponent/StylesItem.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
-
 const StylesItem = (props) => {
   const { style, handleStyles, handleCurrentPhoto, currentStyle, handleThumbnails } = props
+
   return (
     <>
       <img className="styles-thumbnails" style={currentStyle === style ? { border: '1px solid red' } : { border: '1px solid black' }} src={`${style.photos[0].thumbnail_url}`} alt={`${style.name}`} onClick={(e) => {
