8096c8a301cf3de970d857f1b77e0cd7ff2d69d9
Nick Amenda <104174057+nickamenda@users.noreply.github.com>
2022-10-26 18:52:25 -0500

    Change Back

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,7 @@ module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
-    path: DIST_DIR,
-    publicPath: '/'
+    path: DIST_DIR
   },
   devtool: 'source-map',
   module: {
